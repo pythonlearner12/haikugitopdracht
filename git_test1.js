554ea615a4b453ea31b179e195a36b15a12e62d8
Fred <36563@ma-web.nl>
2023-10-09 10:24:38 +0200

    auther

--- git_test1.js
+++ git_test1.js
@@ -1,17 +1,17 @@
 function HaikuX1(){
-     console.log("haiku by:","AUTHOR");
+     console.log("haiku by:","Kobayahsi Issa");
      console.log("The wren Earns his living Noiselessly.");
      console.log("From time to time The clouds give rest To the moon-beholders.");
      console.log("Over-ripe sushi, The Master Is full of regret.");
 }
 function HaikuX2(){
-    console.log("haiku by:","AUTHOR");
+    console.log("haiku by:","Matsuo Bashō");
     console.log("Consider me As one who loved poetry And persimmons.");
     console.log("In the cicada's cry No sign can foretell How soon it must die.");
     console.log("Blowing from the west Fallen leaves gather In the east.");
 }
 function HaikuX3(){
-    console.log("haiku by:","AUTHOR");
+    console.log("haiku by:","Yosa Buson");
     console.log("Winter seclusion - Listening, that evening, To the rain in the mountain.");
     console.log("Don’t weep, insects – Lovers, stars themselves, Must part.");
     console.log("My life, - How much more of it remains? The night is brief.");
@@ -19,4 +19,3 @@ function HaikuX3(){
 HaikuX1()
 HaikuX2()
 HaikuX3()
-dfghj   
