bbbe504c538f6fcb2f2dfed81cbaeedbf304ee75
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-10-09 10:18:50 +0200

    eerste bewerking

--- git_test1.js
+++ git_test1.js
@@ -1,8 +1,8 @@
 function HaikuX1(){
      console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+     console.log("The wren Earns his living Noiselessly.");
+     console.log("From time to time The clouds give rest To the moon-beholders.");
+     console.log("Over-ripe sushi, The Master Is full of regret.");
 }
 function HaikuX2(){
     console.log("haiku by:","AUTHOR");
